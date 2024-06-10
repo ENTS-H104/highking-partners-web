d1a2e9d56c9962f1cce4d95c1b2afa6c5e584cc0
daf2a <muhammaddaffaashdaqfillah@gmail.com>
2024-06-10 22:46:41 +0700

    rm: props

--- app/page.tsx
+++ app/page.tsx
@@ -72,7 +72,7 @@ export default function Component() {
       </main>
       <footer className="fixed bottom-0 left-0 right-0 py-6 w-full flex flex-col gap-2 sm:flex-row items-center px-4 md:px-6 border-t bg-white z-10">
         <p className="text-xs text-gray-500 dark:text-gray-400">
-          &copy; 2024 HighKing. All rights reserved.
+          2024 HighKing. All rights reserved.
         </p>
         <nav className="sm:ml-auto flex gap-4 sm:gap-6">
           <Link
