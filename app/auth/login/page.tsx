1d024d8ae2e56cd9870795eba40d5f4d72b11139
daf2a <muhammaddaffaashdaqfillah@gmail.com>
2024-06-11 05:43:30 +0700

    fix: layout login register

--- app/auth/login/page.tsx
+++ app/auth/login/page.tsx
@@ -69,7 +69,7 @@ const Login = () => {
         }}
         className=""
       >
-        <div className="max-w-md w-full md:mx-auto rounded-none md:rounded-2xl p-4 mx-7 md:p-8 shadow-input bg-white dark:bg-black">
+        <div className="w-full md:max-w-md max-w-xs mx-7 md:mx-auto rounded-none md:rounded-2xl p-4 md:p-8 shadow-input bg-white dark:bg-black">
           <h2 className="font-bold text-xl text-neutral-800 dark:text-neutral-200">
             Welcome to HighKing
           </h2>
